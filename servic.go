58cfc6f5bbd2b86b2cdf71050e44690fd9f32c7e
ggobbe <guillaume.gobbe@gmail.com>
2015-02-14 11:02:26 +0000

    Arguments validation and error handling

--- servic.go
+++ servic.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -11,14 +12,22 @@ import (
 func main() {
 	argsLen := len(os.Args)
 	if argsLen < 2 || argsLen > 3 {
-		fmt.Println("Usage: servic [folder] ([port])")
+		fmt.Printf("Usage: %s [dir] ([port])\n", os.Args[0])
 		return
 	}
 	staticFolder := os.Args[1]
 	port := 8080
+	if argsLen > 2 {
+		p, err := strconv.Atoi(os.Args[2])
+		if err != nil || p < 1 || p > 65535 {
+			fmt.Println("port isn't in range [1-65535]")
+			return
+		}
+		port = p
+	}
 	exists, err := exists(staticFolder)
 	if err != nil {
-		fmt.Printf("Error whilst checking if directory '%s' exists: %s\n", staticFolder, err)
+		fmt.Printf("Error whilst checking if directory '%s' exists\n  %s\n", staticFolder, err)
 		return
 	}
 	if !exists {
@@ -28,7 +37,9 @@ func main() {
 	r := mux.NewRouter()
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir(staticFolder)))
 	http.Handle("/", r)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		fmt.Printf("Error listening on port %d\n  %s\n", port, err)
+	}
 }
 
 func exists(path string) (bool, error) {
